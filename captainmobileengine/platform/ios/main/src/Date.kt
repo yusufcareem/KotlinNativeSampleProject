5416733892b3deef0aec14e69dbdedbbc7aad14d
Yusuf Afghan <yusuf.afghan@careem.com>
2018-03-26 18:10:24 +0400

    Added NSDate() method

--- captainmobileengine/platform/ios/main/src/Date.kt
+++ captainmobileengine/platform/ios/main/src/Date.kt
@@ -4,6 +4,6 @@ import platform.Foundation.*
 
 actual class Date actual constructor () {
     actual companion object {
-        actual val current: Double = NSDate.timeIntervalSince1970()
+        actual val current: Double = NSDate().timeIntervalSince1970()
     }
 }
